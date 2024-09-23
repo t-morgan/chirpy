8820b11850483c473220b1d350fa89aafaf33f23
tmorgan1 <tmorgan1@netjets.com>
2024-09-23 10:01:04 -0400

    healthz endpoint

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handleHealthz)
 
 	var srv http.Server
 	srv.Handler = mux
 	srv.Addr = ":8080"
 	
 	srv.ListenAndServe()
+}
+
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
 }
